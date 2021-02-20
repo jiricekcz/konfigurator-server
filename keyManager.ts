064503a9c254b6a38c93c16920536537d732aa00
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-02-20 22:44:32 +0100

    cloning data for keys

--- keyManager.ts
+++ keyManager.ts
@@ -5,13 +5,15 @@ export const keys: any = {};
 export function createKey(data: SocketData): string {
     var key = crypto.randomBytes(64).toString("base64");
     while(keys[key] !== undefined) key = crypto.randomBytes(64).toString("base64");
-    keys[key] = data;
+    keys[key] = Object.assign<EmptyObject, SocketData>({}, data);
     return key;
 }
 export function getDataFromKey(key: string): SocketData {
     return keys[key] || {};
 }
+interface EmptyObject {
 
+}
 
 interface SocketData {
     email?: string;
